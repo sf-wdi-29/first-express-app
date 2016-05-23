a0edaecbba06b80f571f1a730cf1e8f5e2994434
Ben Hulan <benhulan@gmail.com>
2016-05-23 16:21:40 -0700

    initial commit on video branch

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const taquerias = [
-{restaurant: "La Taqueria", id: 1},
-{restaurant: "El Farolito", id: 2},
-{restaurant: "Taqueria Cancun", id: 3}
+    {restaurant: "La Taqueria", id: 14},
+    {restaurant: "El Farolito", id: 23},
+    {restaurant: "Taqueria Cancun", id: 35}
 ];
 
+app.use('views', './views');
+app.use('view engine', 'ejs');
+
 app.get('/', function(req, res){
+    console.log("hello from root route");
     res.send("You're home \n");
 });
 
